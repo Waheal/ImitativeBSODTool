48f613bdf76fcd83ba8e53ff3aafd4072da08df7
Waheal <2035582067@qq.com>
2024-10-19 14:03:42 +0800

    update

--- ImitativeBSODTool/MainForm.Designer.cs
+++ ImitativeBSODTool/MainForm.Designer.cs
@@ -229,7 +229,6 @@ namespace ImitativeBSODTool
             // 
             // button2
             // 
-            this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button2.Location = new System.Drawing.Point(312, 75);
             this.button2.Name = "button2";
